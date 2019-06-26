d0d6caa889864f9e11823bafe7b9dedbc6cd10f9
Greg Dardis <Greg.Dardis.03@pleasantsolutions.com>
2019-06-25 17:05:17 -0700

    Finished basic implementation, displays tuples containing characters and their counts, sorted by count

--- counter.py
+++ counter.py
@@ -2,6 +2,22 @@
 # Then instantiate a class depending on which file path we are given so we can count properly in different file types using the same API
 # UNLESS we just want to handle text files in this little project
 
+def get_letter_counts(file):
+    counts = {}
+    for line in file:
+        for char in line:
+            if char in counts:
+                counts[char] += 1
+            else:
+                counts[char] = 1
+    return counts
+
+def get_letter_counts_list(counts_map):
+    counts_list = []
+    for letter in counts_map:
+        counts_list.append((letter, counts_map[letter]))
+    return counts_list
+
 if __name__ == '__main__':
     print('\nWelcome to Character Counter!\n')
     print('Pass me a text file, and I will tell you how many of each character are in it.')
@@ -18,10 +34,15 @@ if __name__ == '__main__':
             print("File not found. Make sure your file path is correct.")
             continue
 
-        print(file.read())
+        counts_map = get_letter_counts(file)
+
+        # list of tuples counting (char, count) to be used for sorting
+        unsorted_letter_counts = get_letter_counts_list(counts_map)
+        sorted_letter_counts = sorted(unsorted_letter_counts, key=lambda tup: tup[1], reverse=True)
+
+        print('The number of each letter in {} from largest to smallest is:\n'.format(file.name))
+        print(sorted_letter_counts)
+
+        file.close()
         break
-
-
-
-
 
