34f9c5ad378ed0bc0fff6582068bbd95654b9b24
Greg Dardis <Greg.Dardis.03@pleasantsolutions.com>
2019-06-26 10:18:08 -0700

    Last little cleanup

--- counter.py
+++ counter.py
@@ -1,23 +1,23 @@
-#TODO: add comments to methods
 from colorama import Fore
 from colorama import Style
 
 class CharacterCounter():
-    '''Counts characters in a .txt file and displays them to the console\n
+    """Counts characters in a .txt file and displays them to the console\n
     
     Raises:
         IOError: file path doesn't exist
-    '''
+    """
     def __init__(self, file_path):
         try:
-            self.file = open(file_path, 'r')
+            file = open(file_path, 'r')
+            self.file = file
         except:
             raise IOError("File not found. Make sure your file path is correct.")
-
+            
     def display_char_counts(self):
-        '''
+        """
         Displays the number of each character in the text file to the console.
-        '''
+        """
         sorted_char_counts = self._get_sorted_char_counts()
 
         print(f'\n{Style.BRIGHT}{Fore.GREEN}The number of each character in {self.file.name} from largest to smallest is:{Style.RESET_ALL}')
@@ -28,7 +28,6 @@ class CharacterCounter():
             else:
                 print(f'{Style.BRIGHT}{Fore.WHITE}{char}: {count}{Style.RESET_ALL}')
                 
-
     def _count_chars(self):
         counts = {}
         for line in self.file:
@@ -38,14 +37,12 @@ class CharacterCounter():
 
     def _get_sorted_char_counts(self):
         char_counts = self._count_chars()
-
         unsorted_char_counts = []
         for letter in char_counts:
             unsorted_char_counts.append((letter, char_counts[letter]))
 
         return sorted(unsorted_char_counts, key=lambda tup: tup[1], reverse=True)
 
-# Demo
 if __name__ == '__main__':
     print(f'\n{Style.BRIGHT}{Fore.GREEN}Welcome to Character Counter!{Style.RESET_ALL}\n')
     print('Pass me a text file, and I will tell you how many of each character are in it.')
@@ -62,5 +59,3 @@ if __name__ == '__main__':
             print(error.args[0])
 
     counter.display_char_counts()
-    # file.close()#TODO: close file within CharacterCounter? Automatically close it or expose API for user to close it
-
